c813bfdf6fbd96593fe4773c12545b6bb0e4b79f
Morteza <moorycool12@gmail.com>
2026-08-30 16:46:39 +0330

    create apps

---
+++ core/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.ImageField(nall=True, blank=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')])
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
