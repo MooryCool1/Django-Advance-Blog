b5d8818aaaa098589a20ccf756d449a44e84b503
Morteza <moorycool12@gmail.com>
2026-08-30 17:37:50 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,52 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)    
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User model
+
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    is_superuser = models.BooleanField(default=False)   
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    #is_verified = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=20)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
